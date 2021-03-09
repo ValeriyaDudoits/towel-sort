0e4b69eb62e9f5578dfac0490090b07e1568baba
unknown <lerk1512@gmail.com>
2021-03-09 21:16:40 +0300

    fix: change condition

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 module.exports = function towelSort(matrix) {
   let result = [];
-  if (matrix === []) return [];
+  if (matrix === undefined) return [];
   for (let i = 0; i < matrix.length; i++) {
     if (i % 2 != 0) {
       matrix[i].reverse();
